45a3a540a1787cdf608f3399d3b494c84e5dd556
PetarTolev <petar.dtolev@gmail.com>
2020-02-14 11:18:39 +0200

    Add enums, add Product model.

---
+++ Exam Preparation/CSharp-Web-Basics-Exam-Preparation-Part-1-Resources/Andreys/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿namespace Andreys.Models
+{
+    using Enums;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Product
+    {
+        [Key]
+        public string Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public Category Category { get; set; }
+
+        [Required]
+        public Gender Gender { get; set; }
+    }
+}
