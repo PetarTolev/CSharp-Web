c681bcc9b9881c0789fd04f02de395df083e3cff
PetarTolev <petar.dtolev@gmail.com>
2020-02-16 09:32:42 +0200

    Add controllers.

---
+++ Exam/SharedTrip/Controllers/TripsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using SIS.HTTP;
+using SIS.MvcFramework;
+
+namespace SharedTrip.Controllers
+{
+    public class TripsController : Controller
+    {
+        public TripsController()
+        {
+            
+        }
+
+        public HttpResponse All()
+        {
+            throw new NotImplementedException();
+        }
+
+        public HttpResponse Details()
+        {
+            throw new NotImplementedException();
+        }
+
+        public HttpResponse Add()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public HttpResponse Add(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Exam/SharedTrip/Controllers/UsersController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using SIS.HTTP;
+using SIS.MvcFramework;
+
+namespace SharedTrip.Controllers
+{
+    public class UsersController : Controller
+    {
+        public UsersController()
+        {
+            
+        }
+
+        public HttpResponse Login()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public HttpResponse Login(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public HttpResponse Logout()
+        {
+            throw new NotImplementedException();
+        }
+
+        public HttpResponse Register()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public HttpResponse Register(string username)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
