96cc617b71d2daca292908276f3ebc0d583cf312
PetarTolev <petar.dtolev@gmail.com>
2020-03-07 22:42:23 +0200

    Add dropdown contest button. Add contests view model.

---
+++ ASP.NET Core/LearnLanguageSystem/Web/LearnLanguageSystem.Web.ViewModels/Contests/ContestViewModel.cs
@@ -0,0 +1,14 @@
+﻿using LearnLanguageSystem.Data.Models.Contest;
+using LearnLanguageSystem.Services.Mapping;
+
+namespace LearnLanguageSystem.Web.ViewModels.Contests
+{
+    using System.Collections.Generic;
+
+    using LearnLanguageSystem.Web.ViewModels.Questions;
+
+    public class ContestViewModel : IMapFrom<Contest>
+    {
+        public IEnumerable<QuestionViewModel> Questions { get; set; }
+    }
+}
--- ASP.NET Core/LearnLanguageSystem/Web/LearnLanguageSystem.Web/Controllers/ContestsController.cs
+++ ASP.NET Core/LearnLanguageSystem/Web/LearnLanguageSystem.Web/Controllers/ContestsController.cs
@@ -1,11 +1,26 @@
-﻿namespace LearnLanguageSystem.Web.Controllers
+﻿using System.Linq;
+using LearnLanguageSystem.Data;
+using LearnLanguageSystem.Services.Mapping;
+using LearnLanguageSystem.Web.ViewModels.Contests;
+using Microsoft.AspNetCore.Identity.UI.V3.Pages.Internal.Account.Manage;
+
+namespace LearnLanguageSystem.Web.Controllers
 {
     using Microsoft.AspNetCore.Mvc;
 
     public class ContestsController : Controller
     {
+        private readonly ApplicationDbContext db;
+
+        public ContestsController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
         public IActionResult Index()
         {
+            //var contest = this.db.Contests.To<ContestViewModel>().First();
+
             return this.View();
         }
     }
